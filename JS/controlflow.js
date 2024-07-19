0a1d44f27a1b998f59a54b2d5ba854ed6e97cc84
aswin-haridas <aswinharidas0@gmail.com>
2024-07-19 12:47:27 +0530

    commit

---
+++ JS/controlflow.js
@@ -0,0 +1,18 @@
+//conditional statements
+
+//if
+
+let temp = 28;
+
+if (temp < 30) {
+    console.log("turn off ac");
+}
+
+// if - else
+
+if (temp = 28) {
+    console.log("optimal temp");
+} else {
+    console.log("adjust heat settings");
+}
+
---
+++ JS/variable.js
@@ -0,0 +1,24 @@
+// let y=10;
+// console.log(y);
+
+// var 
+// function examplefn(){
+//     var x = 10;
+//     if (true) {
+//         var x = 20;
+//         console.log(x);
+//     }
+//     console.log(x);
+// }
+
+// examplefn();
+
+
+// const x = 10;
+// console.log(x)
+
+// if (true) {
+//     const x = 20;
+//     console.log(x);
+// }
+// console.log(x);
